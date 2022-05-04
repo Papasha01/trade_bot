542e5459a0e1fb2dab4a8f09a24570f8a57bfd83
Papasha <Pavelevikin@yandex.ru>
2022-05-04 15:02:06 +0300

    Существенные подвижки

--- bot.py
+++ bot.py
@@ -1,24 +1,52 @@
-from itertools import count
 import time
 import datetime
 from binance.spot import Spot as Client
 import unicorn_binance_websocket_api
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
+from threading import Thread
+
+df = pd.DataFrame()
+
+def get_history(df):
+    coin = 'BTCUSDT'
+    interval = '1m'
+    limit = 1000
+    dt_start = datetime.datetime(2022,5,4, 12)
+    dt_start_ = round(time.mktime(dt_start.timetuple())*1000)
+
+    spot_client = Client(base_url="https://api1.binance.com")
+    klines = spot_client.klines(coin, interval, startTime = dt_start_, limit = limit)
+    df = pd.DataFrame(klines)
+    df.columns = ['Opentime', 'Open', 'High', 'Low', 'Close', 'Volume', 'Closetime', 'Quote asset volume', 'Number of trades','Taker by base', 'Taker buy quote', 'Ignore']
+    df["Close"] = df.Close.astype(float)
+    df.drop(['Open', 'High', 'Low', 'Volume', 'Closetime', 'Quote asset volume', 'Number of trades','Taker by base', 'Taker buy quote', 'Ignore'], axis = 1, inplace = True)
+    return df
 
 
-coin = 'BTCUSDT'
-interval = '1d'
-limit = 1000
-dt_start = datetime.datetime(2022,1,1)
-dt_start_ = round(time.mktime(dt_start.timetuple())*1000)
-
-spot_client = Client(base_url="https://api1.binance.com")
-klines = spot_client.klines(coin, interval, startTime = dt_start_, limit = limit)
-
-df = pd.DataFrame(klines)
-df.columns = ['Opentime', 'Open', 'High', 'Low', 'Close', 'Volume', 'Closetime', 'Quote asset volume', 'Number of trades','Taker by base', 'Taker buy quote', 'Ignore']
-df["Close"] = df.Close.astype(float)
-df.plot(x="Opentime", y="Close")
-print(df.head())
-plt.show()
+ubwa = unicorn_binance_websocket_api.BinanceWebSocketApiManager(exchange="binance.com")
+ubwa.create_stream('kline_1m', "BTCUSDT")
+jsMessage_last = {'data':'k'}
+jsMessage_last['data'] = {'k':'t', 'k':'c'}
+jsMessage_last['data']['k'] = {'t':0, 'c':0}
+while True:
+    oldest_data_from_stream_buffer = ubwa.pop_stream_data_from_stream_buffer()
+    if oldest_data_from_stream_buffer:
+        jsMessage = json.loads(oldest_data_from_stream_buffer)
+        if 'stream' in jsMessage.keys():
+            if jsMessage['data']['k']['t'] > jsMessage_last['data']['k']['t']:
+                jsMessage_last = jsMessage
+                df = get_history(df)
+                print()
+                print(df)
+            else:
+                print(df.tail(3))
+                # df.iloc[df.last_valid_index()] = pd.DataFrame.from_dict({'Opentime': [jsMessage_last['data']['k']['t']], 'Close': [jsMessage_last['data']['k']['c']]})
+                df.iloc[df.last_valid_index(), 0] = jsMessage['data']['k']['t']
+                df.iloc[df.last_valid_index(), 1] = jsMessage['data']['k']['c']
+                print(df.tail(3))
+                df.plot(x = 'Opentime', y = 'Close')
+                plt.show()
+                
+    else: time.sleep(1)
--- test.py
+++ test.py
@@ -1,14 +1,31 @@
-from itertools import count
-import time
-import datetime
-from binance.spot import Spot as Client
-import unicorn_binance_websocket_api
+# Импорт пакета панд
+
 import pandas as pd
-import matplotlib.pyplot as plt
 
+  
+# создать словарь по пять полей в каждом
 
-download_url = ("https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv")
-df = pd.read_csv(download_url)
-df.info()
-df.plot(x="Rank", y=["P25th", "Median", "P75th"])
-plt.show()
+data = {
+
+    'A':['A1', 'A2', 'A3', 'A4', 'A5'], 
+
+    'B':['B1', 'B2', 'B3', 'B4', 'B5'], 
+
+    'C':['C1', 'C2', 'C3', 'C4', 'C5'], 
+
+    'D':['D1', 'D2', 'D3', 'D4', 'D5'], 
+
+    'E':['E1', 'E2', 'E3', 'E4', 'E5'] }
+
+  
+# Конвертировать словарь в DataFrame
+
+df = pd.DataFrame(data)
+
+  
+# Удалить три столбца в качестве базы индекса
+
+df.drop(df.columns[[0, 4, 2]], axis = 1, inplace = True)
+
+  
+print(df)
